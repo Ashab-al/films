135d71702566c6048bfc304071b6ece1b7696728
Ashab <77648621+Ashab-al@users.noreply.github.com>
2023-04-22 22:27:55 +0300

    Update film.py

--- film.py
+++ film.py
@@ -1,16 +1,18 @@
+
+
 class Film:
+
+    @staticmethod
+    #Вывод жанров в терминал
+    def genre(data_genre:list):
+        for id, genre in data_genre:
+            print(f"{id}. {genre}")
+
     def __init__(self, film_hash):
         self.title = film_hash["nameRu"]
         self.year = film_hash["year"]
-
+        genre = []
 
     def __str__(self):
         return f"Название: {self.title}\nГод: {self.year}"
     
-class Genres:
-    def __init__(self, genre_hash):
-        self.id = genre_hash["id"]
-        self.genre = genre_hash["genre"]
-    
-    def __str__(self):
-        return f"{self.id}. {self.genre}"
